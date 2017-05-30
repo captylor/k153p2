df2ff6473c519631fe3b65e28315e5253472337b
SungWook <syyoo991@naver.com>
2017-05-30 18:51:27 +0900

    테스트2번!

---
+++ k153p2/src/com/kosta/k153p2/init2/ctrl/test22.java
@@ -0,0 +1,22 @@
+package com.kosta.k153p2.init2.ctrl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member.do")
+public class test22 extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("asd");
+		
+		
+	}
+	
+}
